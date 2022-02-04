c868bebb95db0622aac90db81e9877ced185a57c
nitintilwani <70369990+nitintilwani@users.noreply.github.com>
2022-02-04 17:20:07 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,11 +3,7 @@ length=$(ls -al /home/nitin | grep pythonjenkins | wc -l)
 if [ $length -eq 0 ]
 then
 	sudo python3 -m venv /home/nitin/pythonjenkins
-# 	/home/nitin/pythonjenkins/bin/python3 a.py
-	ls
-	pwd
-	echo $(ls)
-	echo $(pwd)
+	/home/nitin/pythonjenkins/bin/python3 a.py
 
 
 
